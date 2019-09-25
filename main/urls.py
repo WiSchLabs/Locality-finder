97593a31a0927a938b3e5ccc3ecf0628e88166f1
Michael Wittig <948604+Sh4kE@users.noreply.github.com>
2019-09-25 22:33:22 +0200

    User handling functions and views
    Toasts are not yet shown due to improper loading...

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "main"
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
