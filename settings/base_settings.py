52a9bcb1e794706889d5cec6b824d410afe36d55
Sh4kE <948604+Sh4kE@users.noreply.github.com>
2019-10-06 23:09:32 +0200

    some better defaults

--- settings/base_settings.py
+++ settings/base_settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'locality_finder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'db_password',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
