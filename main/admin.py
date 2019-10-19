64e42a7d2b116db9c1c29b23c72cc8568fa8a41e
Sh4kE <948604+Sh4kE@users.noreply.github.com>
2019-10-19 14:55:21 +0200

    WIP: categories

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib.gis import admin
 from leaflet.admin import LeafletGeoAdmin
 
-from .models import WorldBorder
+from .models import WorldBorder, Category
 
 admin.site.register(WorldBorder, LeafletGeoAdmin)
+admin.site.register(Category)
--- main/models.py
+++ main/models.py
@@ -2,6 +2,31 @@ from django.contrib.gis.db import models
 from django.utils.text import slugify
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.SET_NULL)
+
+    class Meta:
+        # enforcing that there can not be two categories under a parent with same slug
+        # __str__ method elaborated later in post.  use __unicode__ in place of
+        # __str__ if you are using python 2
+        unique_together = ('slug', 'parent',)
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        full_path = [self.name]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.name)
+            k = k.parent
+        return ' -> '.join(full_path[::-1])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+
 class WorldBorder(models.Model):
     # Regular Django fields corresponding to the attributes in the
     # world borders shapefile.
@@ -22,9 +47,22 @@ class WorldBorder(models.Model):
 
     worldborder_slug = models.SlugField(blank=True)
 
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
     def save(self, *args, **kwargs):
         self.worldborder_slug = slugify(self.name)
         super(WorldBorder, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+
+    def get_cat_list(self):
+        k = self.category  # for now ignore this instance method
+
+        breadcrumb = ["dummy"]
+        while k is not None:
+            breadcrumb.append(k.slug)
+            k = k.parent
+        for i in range(len(breadcrumb) - 1):
+            breadcrumb[i] = '/'.join(breadcrumb[-1:i - 1:-1])
+        return breadcrumb[-1:0:-1]
